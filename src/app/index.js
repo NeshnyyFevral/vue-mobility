befbe7f1fc3bd295892984e9e874027db6c0947a
Иван Туровский <ivan.turovskiy@gmail.com>
2022-07-01 13:26:18 +0300

    added eslint plugins

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,14 @@
 import { createApp as _createApp } from 'vue';
+
+// import { createPinia } from 'pinia';
 import App from '../App.vue';
 
 export default function createApp() {
+	// const pinia = createPinia();
 	const app = _createApp(App);
 
 	return {
+		// pinia,
 		app,
 	};
 }
